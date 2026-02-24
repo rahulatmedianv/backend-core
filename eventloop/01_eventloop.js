51a11c76ea24fb6b8c70a6672fb94a7806f54886
Rahul pratap singh <263541362+rahulatmedianv@users.noreply.github.com>
2026-02-24 11:29:23 +0530

    add comments

--- eventloop/01_eventloop.js
+++ eventloop/01_eventloop.js
@@ -1,21 +1,18 @@
-console.log('Running on call stack');
+console.log("Running on call stack"); // running on call stack
 
 setTimeout(() => {
-    console.log('3 Running on macro task');
-    Promise.resolve().then(() => {
-        console.log('4 macro task -> micro task');
-        setTimeout(() => {
-            console.log('macro -> micro -> macro');
-        }, 0);
-    })
+  console.log("3 Running on macro task"); // running on macro task
+  Promise.resolve().then(() => {
+    console.log("4 macro task -> micro task"); // from macro task que -> micro/priority task queue
+    setTimeout(() => {
+      console.log("macro -> micro -> macro"); // micro task queue -> macro task queue
+    }, 0);
+  });
 }, 0);
 
 Promise.resolve().then(() => {
-    console.log('2 Running on priority/micro que');
-    setTimeout(() => {
-        console.log('5 micro task que -> macro task');
-    }, 0);
+  console.log("2 Running on priority/micro que"); // micro task queue
+  setTimeout(() => {
+    console.log("5 micro task que -> macro task"); // micro task -> macro task
+  }, 0);
 });
-
-
-
